25fee3c970502b4ab67b3caef0fc403fd91e4441
kenmatsumoto <ken@nri.com>
2017-12-12 18:44:49 -0800

    async problem seems solved

--- index.js
+++ index.js
@@ -8,18 +8,19 @@
 'use strict';
 // firebase setting
 var https = require('https'); // for synchonous call
+//var request = require('sync-request');
 var firebase = require('firebase');
-//var admin = require("firebase-admin");
+var admin = require("firebase-admin");
 var async = require("async");
 var serviceAccount = require("./quickstart-nodejs-a3153-firebase-adminsdk-miz6k-a79aa0ff05.json");
 const firebaseHost = "quickstart-nodejs-a3153.firebaseio.com";
-/*
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://quickstart-nodejs-a3153.firebaseio.com"
 });
 
-*/
+
 
   // Initialize Firebase
   var config = {
@@ -30,15 +31,17 @@ admin.initializeApp({
     storageBucket: "quickstart-nodejs-a3153.appspot.com",
     messagingSenderId: "387337038873"
   };
-  firebase.initializeApp(config);
+ // firebase.initializeApp(config);
 
 
 exports.handler = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false; 
     // TODO implement
     console.log('firing...');
     var speechOutput = "test from lambda.";
     var username = "ken";
-/*     var database = admin.database();
+    /*
+    var database = admin.database();
     var ref = database.ref("posts");
     //var key = ref.push();
     var uid = 'YBjxSHLFySS79Uaa78Gr1LQV83h2';
@@ -52,8 +55,8 @@ exports.handler = (event, context, callback) => {
         title: username,
         time : dt.toTimeString(),
         starCount: 0
-    };  */
-/* 
+    };  
+ 
     async.series ([
         function a (callback) {
             ref.push().set(postData);
@@ -68,9 +71,12 @@ exports.handler = (event, context, callback) => {
         }
         console.log("all done. " + results);
     }); 
-    console.log("finished."); */
-    pushFBAsynchronous(speechOutput, username);
-    pushFBSynchronous(speechOutput, username);
+    console.log("finished."); 
+    */
+    var a = pushFBAsync2(speechOutput, username);
+    //pushFBAsynchronous(speechOutput, username);
+    //pushFBSynchronous(speechOutput, username);
+    //pushFBSynch2(speechOutput, username);
     /*
     return ref.push().set(postData).then(function() {
         console.log ("Data saved successfully.");
@@ -78,9 +84,43 @@ exports.handler = (event, context, callback) => {
         console.log ("Data could not be saved.");                  
     });
     */
-    console.log("back to main code");
+    console.log("back to main code :" + a);
     callback(null, 'Hello from Lambda');
 };
+
+function pushFBAsync2(speechOutput, username) {
+    var database = admin.database();
+    var ref = database.ref("posts");
+    //var key = ref.push();
+    var uid = 'YBjxSHLFySS79Uaa78Gr1LQV83h2';
+    var speechOutput = "test from lambda.";
+    var username = "ken";
+    var dt    = new Date();
+    var postData = {
+        author: 'k8.matsumoto',
+        uid: uid,
+        body: speechOutput,
+        title: username,
+        time : dt.toTimeString(),
+        starCount: 0
+    };  
+ 
+    async.series ([
+        function a (callback) {
+            ref.push().set(postData);
+            callback(null,1);
+        },
+        function (callback) {
+            console.log("after set");
+        }
+    ], function (err, results) {
+        if (err) {
+            console.log ("error occurred");
+        }
+        console.log("all done. " + results);
+    }); 
+    console.log("finished."); 
+}
 
 function pushFBAsynchronous(speechOutput, username) {
     var database = firebase.database();
